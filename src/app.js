d1596eb7f5bd6976f4ad0f343903bdeff5ba4777
karemSolis <soliskarem@gmail.com>
2023-09-21 14:00:55 -0300

    socket io funcionando

--- src/app.js
+++ src/app.js
@@ -9,45 +9,33 @@ import { Server } from 'socket.io' //importación de librería socket.io
 
 const app = express(); //aquí la creación de la instancia de la apli express
 const httpServer = app.listen(8080, () => console.log( "servidor en el puerto 8080" )); //definición del puerto http
-const product = new ProductManager(); /*esta variable es la copia de product.routes, pero es de ProductManager y 
-todas sus funcionalidades. averiguar + */
 
-const socketServer = new Server(httpServer);  /*definición de la librería socket.io la cual permite habilitar la camunicación en tiempo real 
+const socketServer = new Server(httpServer);  /*definición de la librería socket.io la cual permite habilitar la camunicación en tiempo real
 entre un servidor (backend) y un cliente (fronted) */
 
 
-socketServer.on("connection", socket => { //establece una conexión 
+socketServer.on("connection", socket => { //establece una conexión
     console.log("Un usuario conectado")
 
     socket.on("message", data => {
         console.log(data)
     })
 
-    socket.on("newProd", (newProduct) => {
-        product.addProducts(newProduct)
-        socketServer.emit("success", "Producto Agregado Correctamente");
-    });
-
-    socket.emit("test","Servidor: Validación en consola del navegador")
-
 })
 
 
-
-
-
-const product = new ProductManager(); /*esta variable es la copia de product.routes, pero es de ProductManager y 
+const product = new ProductManager(); /*esta variable es la copia de product.routes, pero es de ProductManager y
 todas sus funcionalidades. averiguar + */
 
 
-//middleware 
+//middleware
 
-//analizarán solicitudes HTTP entrantes y los convertirán en formato json o url 
+//analizarán solicitudes HTTP entrantes y los convertirán en formato json o url
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 
-socketServer.on("connection", socket => { //establece una conexión 
+socketServer.on("connection", socket => { //establece una conexión
     console.log("Un usuario conectado")
 
         socket.on("message", data=>{
@@ -58,19 +46,19 @@ socketServer.on("connection", socket => { //establece una conexión
 
 
 //estos middlewars son toda la extructura de handlebars
-app.engine("handlebars", engine());  /*acá le digo al servidor que usaremos M.P.handlebars para el uso de express y que será a 
+app.engine("handlebars", engine());  /*acá le digo al servidor que usaremos M.P.handlebars para el uso de express y que será a
 través de engine()*/
 app.set("view engine", "handlebars"); /*acá le digo al server que los archivos de view terminaran con la extensión .handlebars, se establece la vista
 como handlebars, eso significa que express usará handlebars para renderizar las vistas*/
 app.set("views", path.resolve(__dirname + "/views")); /*y además obvio debo decirle donde encontrar esos archivos, estableciendo la ubicación de las vistas
-es una ruta absoluta al directorio de vistas que utiliza __dirname que he importado desde utils.js, así que express buscará en ese directorio las*/                             
+es una ruta absoluta al directorio de vistas que utiliza __dirname que he importado desde utils.js, así que express buscará en ese directorio las*/
 
 
 //middleware para archivos estáticos
 app.use("/", express.static(__dirname + "/public")) /*con __dirname le índico que en puclic estarán los archivos estáticos como el style.css*/
 
-//ruta a la página principal 
-app.get("/", async(req, res) =>{ 
+//ruta a la página principal
+app.get("/", async(req, res) =>{
     let products = await product.getProducts()/*gracias a la constante product copiada y pegada desde product.router puedo reutilizar funciones de rutas hechas ahí */
     res.render("home", { /*este render nos renderizará el archivo handlebars en main, pero a través de lo que hagamos en home */
         title: "handlebars y websockets",
